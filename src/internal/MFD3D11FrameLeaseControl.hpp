f51ad20e49d3c6c59ac01fc320c3bc65e062d4a9
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-08 20:57:45 +0900

    d3d11: add frame lease control

---
+++ src/internal/MFD3D11FrameLeaseControl.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+
+namespace MFFrameSource::internal {
+
+struct MFD3D11FrameLeaseControl {
+    mutable std::mutex mutex;
+    std::condition_variable cv;
+    bool closing = false;
+};
+
+} // namespace MFFrameSource::internal
